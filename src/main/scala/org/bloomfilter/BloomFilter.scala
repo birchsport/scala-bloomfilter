196f324c1c464ea7d55195f77c0049a8fe77c940
James Birchfield <jbirchfield@missiondata.com>
2013-03-20 15:49:23 -0500

    adjusted tests to do multiple scenarios and add new methods to take an Any object to add to the filter

--- src/main/scala/org/bloomfilter/BloomFilter.scala
+++ src/main/scala/org/bloomfilter/BloomFilter.scala
@@ -2,6 +2,9 @@ package org.bloomfilter
 
 import scala.math._
 
+/**
+ * An implementation of the Bloom Filter algorithm (http://en.wikipedia.org/wiki/Bloom_filter)
+ */
 class BloomFilter(val size: Int, val expectedElements: Int) {
   require(size > 0)
   require(expectedElements > 0)
@@ -10,6 +13,17 @@ class BloomFilter(val size: Int, val expectedElements: Int) {
   val k = ceil((bitArray.size / expectedElements) * log(2.0)).toInt
   val expectedFalsePositiveProbability = pow(1 - exp(-k * 1.0 * expectedElements / bitArray.size), k)
 
+  /**
+   * Add any object to the filter.
+   * Will invoke the 'hashCode' method on the passed in object
+   */
+  def add(any: Any) {
+    add(any.hashCode)
+  }
+
+  /**
+   * Add a hash value to the filter
+   */
   def add(hash: Int) {
     def add(i: Int, seed: Int) {
       if (i == k) return
@@ -20,6 +34,17 @@ class BloomFilter(val size: Int, val expectedElements: Int) {
     add(0, hash)
   }
 
+  /**
+   * Checks to see if any object is in the filter.
+   * Will invoke the 'hashCode' method on the passed in object
+   */
+  def contains(any: Any): Boolean = {
+    contains(any.hashCode)
+  }
+
+  /**
+   * Checks to see if a given hash value is in the filter
+   */
   def contains(hash: Int): Boolean = {
     def contains(i: Int, seed: Int): Boolean = {
       if (i == k) return true
--- src/test/scala/org/bloomfilter/BloomFilterSpec.scala
+++ src/test/scala/org/bloomfilter/BloomFilterSpec.scala
@@ -5,12 +5,28 @@ import org.scalatest.matchers.ShouldMatchers
 import scala.io.Source
 
 class BloomFilterSpec extends FlatSpec with ShouldMatchers {
-  val filter = new BloomFilter(1000000, 250000)
+  "Loading words from a dictionary" should "work" in {
+    val filter = new BloomFilter(1000000, 250000)
 
-  val source = Source.fromURL(getClass.getResource("/words.txt"))
-  for (line <- source.getLines()) {
-    filter.add(line.hashCode)
+    val source = Source.fromURL(getClass.getResource("/words.txt"))
+    var i: Int = 0
+    for (line <- source.getLines()) {
+      i = i + 1;
+      if (i % 2 == 0) {
+        filter.add(line.hashCode)
+      } else
+        filter.add(line)
+    }
+    filter.contains("foo".hashCode) should equal(true)
+    filter.contains("foo") should equal(true)
+    filter.contains("bbaarr") should equal(false)
   }
-  filter.contains("foo".hashCode) should equal(true)
-  filter.contains("ffoooo".hashCode) should equal(false)
+
+  "Adding words directly " should "work" in {
+    val filter = new BloomFilter(100, 4)
+
+    filter.add("foo")
+    filter.contains("foo".hashCode) should equal(true)
+    filter.contains("bbaarr") should equal(false)
+  }
 }
